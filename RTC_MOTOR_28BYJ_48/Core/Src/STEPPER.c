21992f9b8aea7a393fa83bc064cde84e936f5f38
이찬솔 <58240527+chansol790@users.noreply.github.com>
2023-09-21 14:18:47 +0900

    23.09.21

---
+++ RTC_MOTOR_28BYJ_48/Core/Src/STEPPER.c
@@ -0,0 +1,102 @@
+/*
+ * STEPPER.c
+ *
+ *  Created on: Sep 21, 2023
+ *      Author: user
+ */
+
+
+#include "STEPPER.h"
+#include "delay.h"
+
+void stepMoterSpeed(int speed)
+{
+  delay_us((60000000/stepMoterHalfStep)/speed);
+}
+
+void stepMoterHalfDrive(int step)
+{
+  uint8_t step1 = step % 8;
+  switch (step1) {
+    case 0:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, RESET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, RESET);
+      break;
+    case 1:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, RESET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, RESET);
+      break;
+    case 2:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, RESET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, RESET);
+      break;
+    case 3:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, RESET);
+      break;
+    case 4:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, RESET);
+      break;
+    case 5:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, SET);
+      break;
+    case 6:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, RESET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, SET);
+      break;
+    case 7:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, RESET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, SET);
+      break;
+  }
+}
+
+void stepMoterFastDrive(int step)
+{
+  uint8_t step1 = step % 4;
+  switch (step1) {
+    case 0:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, RESET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, RESET);
+      break;
+    case 1:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, RESET);
+      break;
+    case 2:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, SET);
+      break;
+    case 3:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, RESET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, SET);
+      break;
+
+  }
+}
